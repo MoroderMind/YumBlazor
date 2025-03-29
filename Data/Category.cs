afdf74344f8f12ea14c2c1dafaa5a2a2643ed0e3
MoroderMind <moroderwow@gmail.com>
2025-03-29 13:28:34 +0100

    created catagory and migrated and seeded database

---
+++ Data/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        public string Name { get; set; }
+    }
+}
